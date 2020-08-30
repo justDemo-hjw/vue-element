7548d55117f28f3ce8741adfab5881d4a28c9eba
hanjiawang <1301671498@qq.com>
2020-08-30 18:12:37 +0800

    头部详情

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <!--
  * @Date: 2020-08-23 22:42:27
  * @LastEditors: hanjiawang
- * @LastEditTime: 2020-08-26 23:38:02
+ * @LastEditTime: 2020-08-30 18:06:02
 -->
 <template>
   <div id="app">
     <VHeader :seller="seller" />
+    <HeaderDetail :seller="seller" />
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
+import HeaderDetail from 'components/header-detail/header-detail'
 import { getSeller } from 'api'
 export default {
   name: 'App',
   components: {
-    VHeader
+    VHeader,
+    HeaderDetail
   },
   data() {
     return {
